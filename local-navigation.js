6ec0646daab15de4ee7e1c023c0f3dda2b1e9b30
Karol Bohdanowicz <bohunek5@gmail.com>
2026-09-08 09:26:36 +0200

    fix(gh-pages): fix broken assets and layout via dynamic base tag and relative paths for GitHub Pages subpath

--- local-navigation.js
+++ local-navigation.js
@@ -7,30 +7,30 @@ document.addEventListener("DOMContentLoaded", () => {
     dockContainer.className = "prescot-dock";
     dockContainer.setAttribute("aria-label", "Nawigacja główna");
     dockContainer.innerHTML = `
-      <a href="/" class="dock-item" data-tooltip="Strona główna" aria-label="Strona główna">
+      <a href="./" class="dock-item" data-tooltip="Strona główna" aria-label="Strona główna">
         <svg class="dock-logo-icon" viewBox="0 0 377.9 257.7" xmlns="http://www.w3.org/2000/svg">
           <path fill="#e14e26" d="M0,0h106.7v50H0V0ZM0,100.9h97.7v48.2H0v-48.2ZM0,206.6h106.7v51.2H0v-51.2h0ZM149.3,100.7h82v48.4h-82v-48.4h0ZM149.3,0h87.4C317.7,0,377.9,42.6,377.9,128.9s-60.1,128.9-141.2,128.9h-87.4v-51.2h90.8c47.8,0,76.6-29.1,76.6-77.7s-27.6-78.8-76.6-78.8h-90.8V0h0Z"/>
         </svg>
       </a>
-      <a href="/produkty/" class="dock-item" data-tooltip="Oferta" aria-label="Oferta">
+      <a href="produkty/" class="dock-item" data-tooltip="Oferta" aria-label="Oferta">
         <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M542.22 32.05c-54.8 3.11-163.72 14.43-230.96 55.59-4.64 2.84-7.27 7.89-7.27 13.17v363.87c0 11.55 12.63 18.85 23.28 13.49 69.18-34.82 169.23-44.32 218.7-46.92 16.89-.89 30.02-14.43 30.02-30.66V62.75c.01-17.71-15.35-31.74-33.77-30.7zM264.73 87.64C197.5 46.48 88.58 35.17 33.78 32.05 15.36 31.01 0 45.04 0 62.75V400.6c0 16.24 13.13 29.78 30.02 30.66 49.49 2.6 149.59 12.11 218.77 46.95 10.62 5.35 23.21-1.94 23.21-13.46V100.63c0-5.29-2.62-10.14-7.27-12.99z"/></svg>
       </a>
-      <a href="/tasmy-led/" class="dock-item" data-tooltip="Taśmy LED" aria-label="Taśmy LED">
+      <a href="tasmy-led/" class="dock-item" data-tooltip="Taśmy LED" aria-label="Taśmy LED">
         <svg viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M224 192c-35.3 0-64 28.7-64 64s28.7 64 64 64 64-28.7 64-64-28.7-64-64-64zm400 224H380.6c41.5-40.7 67.4-97.3 67.4-160 0-123.7-100.3-224-224-224S0 132.3 0 256s100.3 224 224 224h400c8.8 0 16-7.2 16-16v-32c0-8.8-7.2-16-16-16zm-400-64c-53 0-96-43-96-96s43-96 96-96 96 43 96 96-43 96-96 96z"/></svg>
       </a>
-      <a href="/produkcja/" class="dock-item" data-tooltip="Produkcja" aria-label="Produkcja">
+      <a href="produkcja/" class="dock-item" data-tooltip="Produkcja" aria-label="Produkcja">
         <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M475.115 163.781L336 252.309v-68.28c0-18.916-20.931-30.399-36.885-20.248L160 252.309V56c0-13.255-10.745-24-24-24H24C10.745 32 0 42.745 0 56v400c0 13.255 10.745 24 24 24h464c13.255 0 24-10.745 24-24V184.029c0-18.917-20.931-30.399-36.885-20.248z"/></svg>
       </a>
-      <a href="/dystrybucja/" class="dock-item" data-tooltip="Dystrybucja & B2B" aria-label="Dystrybucja & B2B">
+      <a href="dystrybucja/" class="dock-item" data-tooltip="Dystrybucja & B2B" aria-label="Dystrybucja & B2B">
         <svg viewBox="0 0 640 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M128 352H32c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32zm-24-80h192v48h48v-48h192v48h48v-57.59c0-21.17-17.23-38.41-38.41-38.41H344v-64h40c17.67 0 32-14.33 32-32V32c0-17.67-14.33-32-32-32H256c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h40v64H94.41C73.23 224 56 241.23 56 262.41V320h48v-48zm264 80h-96c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32zm240 0h-96c-17.67 0-32 14.33-32 32v96c0 17.67 14.33 32 32 32h96c17.67 0 32-14.33 32-32v-96c0-17.67-14.33-32-32-32z"/></svg>
       </a>
-      <a href="/baza-wiedzy/" class="dock-item" data-tooltip="Baza Wiedzy" aria-label="Baza Wiedzy & FAQ">
+      <a href="baza-wiedzy/" class="dock-item" data-tooltip="Baza Wiedzy" aria-label="Baza Wiedzy & FAQ">
         <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M256 32C132.3 32 32 132.3 32 256s100.3 224 224 224 224-100.3 224-224S379.7 32 256 32zm0 376c-17.7 0-32-14.3-32-32s14.3-32 32-32 32 14.3 32 32-14.3 32-32 32zm42.7-142.1c-13.8 11.2-26.7 21.6-26.7 46.1v10c0 8.8-7.2 16-16 16h-32c-8.8 0-16-7.2-16-16v-14c0-38.4 22.8-56.9 44.4-74.4 14.1-11.4 27.6-22.3 27.6-39.6 0-21.2-18.7-36-44-36-24.6 0-41.9 14.2-46.7 32.5-2.2 8.5-10.4 13.9-19.1 12.3l-30.8-5.6c-9.1-1.7-14.8-10.7-12.4-19.7C180.7 132.2 214.2 104 256 104c53 0 96 34.3 96 82 0 35.8-21.7 61.2-53.3 83.9z"/></svg>
       </a>
       <a href="https://prescot.com.pl/" class="dock-item" data-tooltip="Sklep B2C" aria-label="Sklep B2C" target="_blank" rel="noopener">
         <svg viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M576 216v16c0 13.255-10.745 24-24 24h-8l-26.113 182.788C514.509 462.435 494.257 480 470.37 480H105.63c-23.887 0-44.139-17.565-47.518-41.212L32 256h-8c-13.255 0-24-10.745-24-24v-16c0-13.255 10.745-24 24-24h67.341l106.78-146.821c10.395-14.292 30.407-17.453 44.701-7.058 14.293 10.395 17.453 30.408 7.058 44.701L170.477 192h235.046L326.12 82.821c-10.395-14.292-7.234-34.306 7.059-44.701 14.291-10.395 34.306-7.235 44.701 7.058L484.659 192H552c13.255 0 24 10.745 24 24zM312 392V280c0-13.255-10.745-24-24-24s-24 10.745-24 24v112c0 13.255 10.745 24 24 24s24-10.745 24-24zm112 0V280c0-13.255-10.745-24-24-24s-24 10.745-24 24v112c0 13.255 10.745 24 24 24s24-10.745 24-24zm-224 0V280c0-13.255-10.745-24-24-24s-24 10.745-24 24v112c0 13.255 10.745 24 24 24s24-10.745 24-24z"/></svg>
       </a>
-      <a href="/kontakt/" class="dock-item" data-tooltip="Kontakt" aria-label="Kontakt">
+      <a href="kontakt/" class="dock-item" data-tooltip="Kontakt" aria-label="Kontakt">
         <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 14l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C4.1 366.5-1.8 378.1.8 389.4l24 104C27.3 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-21-18.6-23.4z"/></svg>
       </a>
       <div class="dock-lang-item">
@@ -42,10 +42,10 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // 1. Podświetlanie aktywnej pozycji w menu dock
-  const path = window.location.pathname.replace(/^\/+|\/+$/g, "").toLowerCase();
+  const path = window.location.pathname.replace(/^\/prescotpl\/?/i, "").replace(/^\/+|\/+$/g, "").toLowerCase();
 
   document.querySelectorAll(".prescot-dock .dock-item").forEach((item) => {
-    const href = (item.getAttribute("href") || "").replace(/^\/+|\/+$/g, "").toLowerCase();
+    const href = (item.getAttribute("href") || "").replace(/^\/+|\/+$/g, "").replace(/^\.\/?/, "").toLowerCase();
 
     // Exact or semantic match
     if (href === path ||
@@ -392,6 +392,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // 5. Initialize GTranslate on dynamically created dock if needed
   if (!document.getElementById("gt-wrapper-prescot-global-script") && !window.gtranslateSettings) {
+    const isGH = window.location.hostname.indexOf('github.io') !== -1 || window.location.pathname.startsWith("/prescotpl");
+    const prefix = isGH ? "/prescotpl" : "";
     window.gtranslateSettings = window.gtranslateSettings || {};
     window.gtranslateSettings['prescot-global'] = {
       default_language: "pl",
@@ -402,11 +404,11 @@ document.addEventListener("DOMContentLoaded", () => {
       alt_flags: [],
       float_switcher_open_direction: "top",
       switcher_horizontal_position: "inline",
-      flags_location: "/wp-content/plugins/gtranslate/flags/"
+      flags_location: prefix + "/wp-content/plugins/gtranslate/flags/"
     };
     const gtScript = document.createElement("script");
     gtScript.id = "gt-wrapper-prescot-global-script";
-    gtScript.src = "/wp-content/plugins/gtranslate/js/float.js?ver=3.1.1";
+    gtScript.src = "wp-content/plugins/gtranslate/js/float.js?ver=3.1.1";
     gtScript.defer = true;
     document.body.appendChild(gtScript);
   }
@@ -421,7 +423,7 @@ document.addEventListener("DOMContentLoaded", () => {
       dialog.id = "prescotB2CDialog";
       dialog.className = "prescot-b2c-dialog";
       
-      const isGH = window.location.pathname.startsWith("/prescotpl");
+      const isGH = window.location.hostname.indexOf('github.io') !== -1 || window.location.pathname.startsWith("/prescotpl");
       const bgImgUrl = isGH 
         ? "/prescotpl/wp-content/uploads/2026/03/prescot-shop-bg.webp" 
         : "/wp-content/uploads/2026/03/prescot-shop-bg.webp";
